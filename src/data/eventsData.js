e920016da339e3f88f712227713f735a4ca992a3
Deepesh Gajwa <deepesh.gajwa@gmail.com>
2026-09-13 04:11:57 +0530

    events new pic

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -106,6 +106,16 @@ import img1 from "../assets/images/events/gallery/1.webp";
 import img2 from "../assets/images/events/gallery/2.webp";
 import img3 from "../assets/images/events/gallery/3.webp";
 import img4 from "../assets/images/events/gallery/4.webp";
+import img5 from "../assets/images/events/gallery/5.webp";
+import img6 from "../assets/images/events/gallery/6.webp";
+import img7 from "../assets/images/events/gallery/7.webp";
+import img8 from "../assets/images/events/gallery/8.webp";
+import img9 from "../assets/images/events/gallery/9.webp";
+import img10 from "../assets/images/events/gallery/10.webp";
+import img11 from "../assets/images/events/gallery/11.webp";
+import img12 from "../assets/images/events/gallery/12.webp";
+import img13 from "../assets/images/events/gallery/13.webp";
+import img14 from "../assets/images/events/gallery/14.webp";
 
 export const galleryImages = [
 
@@ -113,5 +123,15 @@ export const galleryImages = [
     img2,
     img3,
     img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
 
 ];
